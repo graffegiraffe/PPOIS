99186d9e97c8c11e918e62a67c2a4b288d503f51
graffegiraffe <katerublevsk@icluod.com>
2024-10-03 18:23:21 +0300

    Initial commit

---
+++ src/test/java/main/java/FifteenPuzzleTest.java
@@ -0,0 +1,18 @@
+package main;
+
+import org.junit.Test;
+
+public class FifteenPuzzleTest {
+
+    @Test
+    public void testMoveTileValidMove() {
+
+    }
+    @Test
+    public void isSolved() {
+    }
+
+    @Test
+    public void printBoard() {
+    }
+}
